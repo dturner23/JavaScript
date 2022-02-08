994899f7a4f8981012e14627c259eca183468747
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-08 13:00:16 -0600

    progress commit

---
+++ Starter Code/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+//value is a piece of data
+//smallest unit of information in JavaScript
+
+let firstName = 'Matilda';    //declaring a variable
+
+//box called firstName - put Jonas into that box
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//cant write something like this
+
+// let 3years = 3; //cant start variable name with a number
+
+// new is not allowerd in JS - its a reserved keyword in JS
+// function will be an error as well
+
+let name = 'Jonas'; // okay, but not recommended - always use camelCase and dont name it 'name'
+
+// all UPPERCASE is a constant like Pi, e, or Infinity
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// above is better than writing below
+
+let job1 = 'Programmer'; // less descriptive than myFirstJob
+let job2 = 'Teacher';
