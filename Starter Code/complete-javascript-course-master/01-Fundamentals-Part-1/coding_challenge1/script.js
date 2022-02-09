49f66182253b753b7467d2cb9d057e6a3106cee7
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-09 08:54:23 -0600

    template literals

--- Starter Code/complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenge1/script.js
+++ Starter Code/complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenge1/script.js
@@ -22,28 +22,53 @@
 //  Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 // m tall.
 
+
+// Data 1
+
 const markHeightFirst = 1.69
 const markWeightFirst = 78
 const johnHeightFirst = 1.95
 const johnWeightFirst = 92
 
-const markBmiFirst = markWeightFirst / (markHeightFirst * 2) 
-console.log(markBmiFirst);
-const johnBmiFirst = johnWeightFirst / (johnHeightFirst * 2)
-console.log(johnBmiFirst);
+const markBmiFirst = markWeightFirst / (markHeightFirst ** 2) 
+const johnBmiFirst = johnWeightFirst / (johnHeightFirst ** 2)
+console.log(johnBmiFirst, markBmiFirst);
 
 const markHigherBmiFirst = markBmiFirst > johnBmiFirst;
 console.log(markHigherBmiFirst);
+
+
+// Data 2
 
 const markHeightSecond = 1.88
 const markWeightSecond = 95
 const johnHeightSecond = 1.76
 const johnWeightSecond = 85
 
-const markBmiSecond = markWeightSecond / (markHeightSecond * 2) 
-console.log(markBmiSecond);
-const johnBmiSecond = johnWeightSecond / (johnHeightSecond * 2)
-console.log(johnBmiSecond);
+const markBmiSecond = markWeightSecond / (markHeightSecond ** 2) 
+const johnBmiSecond = johnWeightSecond / (johnHeightSecond ** 2)
+console.log(johnBmiSecond, markBmiSecond);
 
 const markHigherBmiSecond = markBmiSecond > johnBmiSecond;
 console.log(markHigherBmiSecond);
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';  // type coercion
+
+console.log(jonas);
+
+//template literals
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log(`String
+multiple
+lines`)
