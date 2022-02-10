cea6ec7ed1bac89041b97a20af8c9ebcbc8d3a34
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-10 05:03:20 -0600

    coding_challenge 5

--- Starter Code/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Starter Code/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -70,14 +70,37 @@
 // console.log(yearsUntilRetirement(1980, 'Bob'));
 
 
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) { 
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;  
+//     return juice; 
+// }
+// console.log(fruitProcessor(2, 3));
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
 }
 
-function fruitProcessor(apples, oranges) { 
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;  
-    return juice; 
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+        }    else {
+            console.log(`${firstName} is already retired`);
+            return `-1`;
+        }
+
+    return retirement;
+    // return `${firstName} retires in ${retirement} years`
 }
-console.log(fruitProcessor(2, 3));
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
