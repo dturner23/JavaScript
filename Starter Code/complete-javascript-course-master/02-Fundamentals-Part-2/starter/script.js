ca6d78299e372e66a0b2d651ef0ca05a6256259f
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-10 09:37:07 -0600

    functions challenge 2

--- Starter Code/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Starter Code/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -82,25 +82,91 @@
 // }
 // console.log(fruitProcessor(2, 3));
 
-const calcAge = function (birthYear) {
-    return 2037 - birthYear;
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if(retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} years`);
+//         return retirement;
+//         }    else {
+//             console.log(`${firstName} is already retired`);
+//             return `-1`;
+//         }
+
+//     return retirement;
+//     // return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+// Array - big container that we can throw variables in for later use
+// const friends = ['Michael', 'Steven', 'Peter'];  // literal sytax array
+// console.log(friends);
+
+// const year = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay'
+// console.log(friends);
+
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+
+// const calcAge = function(birthYear){
+//     return 2037 - birthYear;
+// }
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+// console.log(ages);
+
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+
+//Add elements
+
+const newLength = friends.push('Jay');  // push is a function to call on the array
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John'); 
+console.log(friends);
+
+//Remove elements
+friends.pop();  // removes from the end of the array
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();  // removes from the beginning of the array
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven.');
 }
-
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
-
-    if(retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
-        }    else {
-            console.log(`${firstName} is already retired`);
-            return `-1`;
-        }
-
-    return retirement;
-    // return `${firstName} retires in ${retirement} years`
-}
-
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
